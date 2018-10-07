fc9ac680c399c8a2cc8932ba7b9d5fa6efc4437d
xiaoyao <xiaoyao@atguigu.com>
2018-10-07 22:19:51 +0800

    test coflicts

--- src/main/java/com/atguigu/svn/Person.java
+++ src/main/java/com/atguigu/svn/Person.java
@@ -3,8 +3,7 @@ package com.atguigu.svn;
 public class Person {
 	
 	public static void main(String[] args) {
-		System.out.println("this by left..");
-		System.out.println("this by left..");
+		System.out.println("this by left..");//left
 	}
 
 }
