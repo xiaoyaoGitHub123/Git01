cc32ab54bc00704faee650bb6641c5b838cf3170
xiaoyao <xiaoyao@atguigu.com>
2018-10-07 22:02:17 +0800

    Eclipse中的第一次提交

---
+++ src/main/java/com/atguigu/svn/Person.java
@@ -0,0 +1,10 @@
+package com.atguigu.svn;
+
+public class Person {
+	
+	public static void main(String[] args) {
+		System.out.println("this by left..");
+		System.out.println("this by left..");
+	}
+
+}
